5492fd4da781e3ec017aa897ec5e878486bbecf3
sshind31 <101928160+sshind31@users.noreply.github.com>
2023-10-17 07:28:03 +0530

    Add files via upload

---
+++ MVVM/reservoom-master/src/Reservoom/App.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Reservoom.DbContexts;
+using Reservoom.Exceptions;
+using Reservoom.HostBuilders;
+using Reservoom.Models;
+using Reservoom.Services;
+using Reservoom.Services.ReservationConflictValidators;
+using Reservoom.Services.ReservationCreators;
+using Reservoom.Services.ReservationProviders;
+using Reservoom.Stores;
+using Reservoom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reservoom
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .AddViewModels()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    string connectionString = hostContext.Configuration.GetConnectionString("Default");
+
+                    services.AddSingleton(new ReservoomDbContextFactory(connectionString));
+                    services.AddSingleton<IReservationProvider, DatabaseReservationProvider>();
+                    services.AddSingleton<IReservationCreator, DatabaseReservationCreator>();
+                    services.AddSingleton<IReservationConflictValidator, DatabaseReservationConflictValidator>();
+
+                    services.AddTransient<ReservationBook>();
+
+                    string hotelName = hostContext.Configuration.GetValue<string>("HotelName");
+                    services.AddSingleton((s) => new Hotel(hotelName, s.GetRequiredService<ReservationBook>()));
+
+                    services.AddSingleton<HotelStore>();
+                    services.AddSingleton<NavigationStore>();
+
+                    services.AddSingleton(s => new MainWindow()
+                    {
+                        DataContext = s.GetRequiredService<MainViewModel>()
+                    });
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            ReservoomDbContextFactory reservoomDbContextFactory = _host.Services.GetRequiredService<ReservoomDbContextFactory>();
+            using (ReservoomDbContext dbContext = reservoomDbContextFactory.CreateDbContext())
+            {
+                dbContext.Database.Migrate();
+            }
+
+            NavigationService<ReservationListingViewModel> navigationService = _host.Services.GetRequiredService<NavigationService<ReservationListingViewModel>>();
+            navigationService.Navigate();
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ MVVM/reservoom-master/src/Reservoom/ViewModels/MakeReservationViewModel.cs
@@ -0,0 +1,214 @@
+﻿using Reservoom.Commands;
+using Reservoom.Models;
+using Reservoom.Services;
+using Reservoom.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Reservoom.ViewModels
+{
+    public class MakeReservationViewModel : ViewModelBase, INotifyDataErrorInfo
+    {
+        private string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+
+                ClearErrors(nameof(Username));
+
+                if(!HasUsername)
+                {
+                    AddError("Username cannot be empty.", nameof(Username));
+                }
+
+                OnPropertyChanged(nameof(CanCreateReservation));
+            }
+        }
+
+        private int _floorNumber = 1;
+        public int FloorNumber
+        {
+            get
+            {
+                return _floorNumber;
+            }
+            set
+            {
+                _floorNumber = value;
+                OnPropertyChanged(nameof(FloorNumber));
+
+                ClearErrors(nameof(FloorNumber));
+
+                if (!HasFloorNumberGreaterThanZero)
+                {
+                    AddError("Floor number must be greater than zero.", nameof(FloorNumber));
+                }
+
+                OnPropertyChanged(nameof(CanCreateReservation));
+            }
+        }
+
+        private int _roomNumber;
+        public int RoomNumber
+        {
+            get
+            {
+                return _roomNumber;
+            }
+            set
+            {
+                _roomNumber = value;
+                OnPropertyChanged(nameof(RoomNumber));
+            }
+        }
+
+        private DateTime _startDate = new DateTime(2021, 1, 1);
+        public DateTime StartDate
+        {
+            get
+            {
+                return _startDate;
+            }
+            set
+            {
+                _startDate = value;
+                OnPropertyChanged(nameof(StartDate));
+
+                ClearErrors(nameof(StartDate));
+                ClearErrors(nameof(EndDate));
+
+                if (!HasStartDateBeforeEndDate)
+                {
+                    AddError("The start date cannot be after the end date.", nameof(StartDate));
+                }
+                
+                OnPropertyChanged(nameof(CanCreateReservation));
+            }
+        }
+
+        private DateTime _endDate = new DateTime(2021, 1, 8);
+        public DateTime EndDate
+        {
+            get
+            {
+                return _endDate;
+            }
+            set
+            {
+                _endDate = value;
+                OnPropertyChanged(nameof(EndDate));
+
+                ClearErrors(nameof(StartDate));
+                ClearErrors(nameof(EndDate));
+
+                if (!HasStartDateBeforeEndDate)
+                {
+                    AddError("The end date cannot be before the start date.", nameof(EndDate));
+                }
+
+                OnPropertyChanged(nameof(CanCreateReservation));
+            }
+        }
+
+        public bool CanCreateReservation =>
+            HasUsername &&
+            HasFloorNumberGreaterThanZero &&
+            HasStartDateBeforeEndDate &&
+            !HasErrors;
+
+        private bool HasUsername => !string.IsNullOrEmpty(Username);
+        private bool HasFloorNumberGreaterThanZero => FloorNumber > 0;
+        private bool HasStartDateBeforeEndDate => StartDate < EndDate;
+
+        private string _submitErrorMessage;
+        public string SubmitErrorMessage
+        {
+            get
+            {
+                return _submitErrorMessage;
+            }
+            set
+            {
+                _submitErrorMessage = value;
+                OnPropertyChanged(nameof(SubmitErrorMessage));
+
+                OnPropertyChanged(nameof(HasSubmitErrorMessage));
+            }
+        }
+
+        public bool HasSubmitErrorMessage => !string.IsNullOrEmpty(SubmitErrorMessage);
+
+        private bool _isSubmitting;
+        public bool IsSubmitting
+        {
+            get
+            {
+                return _isSubmitting;
+            }
+            set
+            {
+                _isSubmitting = value;
+                OnPropertyChanged(nameof(IsSubmitting));
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        private readonly Dictionary<string, List<string>> _propertyNameToErrorsDictionary;
+
+        public bool HasErrors => _propertyNameToErrorsDictionary.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public MakeReservationViewModel(HotelStore hotelStore, NavigationService<ReservationListingViewModel> reservationViewNavigationService)
+        {
+            SubmitCommand = new MakeReservationCommand(this, hotelStore, reservationViewNavigationService);
+            CancelCommand = new NavigateCommand<ReservationListingViewModel>(reservationViewNavigationService);
+
+            _propertyNameToErrorsDictionary = new Dictionary<string, List<string>>();
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _propertyNameToErrorsDictionary.GetValueOrDefault(propertyName, new List<string>());
+        }
+
+        private void AddError(string errorMessage, string propertyName)
+        {
+            if (!_propertyNameToErrorsDictionary.ContainsKey(propertyName))
+            {
+                _propertyNameToErrorsDictionary.Add(propertyName, new List<string>());
+            }
+
+            _propertyNameToErrorsDictionary[propertyName].Add(errorMessage);
+
+            OnErrorsChanged(propertyName);
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            _propertyNameToErrorsDictionary.Remove(propertyName);
+
+            OnErrorsChanged(propertyName);
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+    }
+}
